daf2af01ee9e7c8aadf066a12c37af242866bf7b
Documenter.jl <documenter@juliadocs.github.io>
2021-11-17 19:13:16 +0000

    build based on 682149a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MeasureInterface","category":"page"},{"location":"#MeasureInterface","page":"Home","title":"MeasureInterface","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MeasureInterface.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MeasureInterface]","category":"page"}]
+}
